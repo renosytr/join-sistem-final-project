9f0057ee2807957c17d129fe506d818bd5d9fd23
Reno Ardian Syaputra <reno.ardian@aqi.co.id>
2024-09-25 20:02:16 +0700

    menambahkan file project

---
+++ src/App.vue
@@ -0,0 +1,28 @@
+<script setup>
+</script>
+
+<template>
+  <header class="flex items-center justify-between px-32 py-6">
+    <img class="h-full" src="@/assets/img/logo-brand.png" alt="logo join sistem">
+    <ul class="flex">
+      <li class="mx-3">
+        <a href="#">Home</a>
+      </li>
+      <li class="mx-3">
+        <a href="#">Blog</a>
+      </li>
+      <li class="mx-3">
+        <a href="#">About Us </a>
+      </li>
+      <li class="mx-3">
+        <a href="#">Contact Us</a>
+      </li>
+    </ul>
+    <button type="button" class="bg-primary px-8 py-3 rounded-full text-white font-bold">
+      Contact Us
+    </button>
+  </header>
+  <main>
+
+  </main>
+</template>
