7acaaf8c4ccd55dfa9464c0abdd881689aacb007
Reno Ardian Syaputra <reno.ardian@aqi.co.id>
2024-09-26 21:08:32 +0700

    fix(option): add option api

--- src/components/ButtonPrimary.vue
+++ src/components/ButtonPrimary.vue
@@ -1,8 +1,20 @@
-<script setup>
-
+<script>
+export default {
+  data(){
+    return {
+      isOpen: false
+    }
+  },
+  methods: {
+    onClick() {
+      this.isOpen = !this.isOpen
+      console.log("click")
+    }
+  }
+}
 </script>
 <template>
-  <button type="button" class="bg-primary p-3 rounded-full text-white">
+  <button @click="onClick()" type="button" class="bg-primary p-3 rounded-full text-white">
     <slot></slot>
   </button>
 </template>
