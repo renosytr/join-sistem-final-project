2f2846d70407de5c8a8318316926abc2282e8d5d
Reno Ardian Syaputra <reno.ardian@aqi.co.id>
2024-09-26 20:41:59 +0700

    feat(responsive): add navbar mobile view

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,17 @@
 <script setup>
+import { ref } from 'vue'
+import ButtonPrimary from '@/components/ButtonPrimary.vue'
+
+const isOpen = ref(false)
+const openPopUp = () => {
+  isOpen.value = !isOpen.value
+}
 </script>
 
 <template>
-  <header class="flex items-center justify-between px-32 py-6">
+  <header class="flex items-center justify-between px-3 lg:px-32 py-6">
     <img class="h-full" src="@/assets/img/logo-brand.png" alt="logo join sistem">
-    <ul class="flex">
+    <ul class="hidden lg:flex">
       <li class="mx-3">
         <a href="#">Home</a>
       </li>
@@ -18,11 +25,16 @@
         <a href="#">Contact Us</a>
       </li>
     </ul>
-    <button type="button" class="bg-primary px-8 py-3 rounded-full text-white font-bold">
+    <button type="button" class="hidden lg:block bg-primary px-3 py-3 rounded-full text-white font-bold">
       Contact Us
     </button>
+    <ButtonPrimary @click="openPopUp()" class="lg:hidden">
+      <img src="@/assets/img/menu-icon.png" class="w-6" alt="Menu Icon">
+    </ButtonPrimary>
   </header>
-  <main>
-
-  </main>
+  <div v-if="isOpen" class="fixed top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 w-5/6 h-32 bg-white z-20 rounded-lg p-2 flex justify-between items-center">
+    <span>Pop Up Content</span>
+    <ButtonPrimary @click="isOpen = false">Close</ButtonPrimary>
+  </div>
+  <div v-if="isOpen" @click="isOpen = false" class="fixed top-0 left-0 w-full h-full bg-black opacity-50 z-10"></div>
 </template>
---
+++ src/components/ButtonPrimary.vue
@@ -0,0 +1,8 @@
+<script setup>
+
+</script>
+<template>
+  <button type="button" class="bg-primary p-3 rounded-full text-white">
+    <slot></slot>
+  </button>
+</template>
